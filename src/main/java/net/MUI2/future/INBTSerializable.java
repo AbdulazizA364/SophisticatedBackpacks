79377896021d43a8c549416e4e1c0779f9153f94
Abdulaziz <Samsuang14z@gmail.com>
2024-05-19 16:59:22 +0300

    step 2

---
+++ src/main/java/net/MUI2/future/INBTSerializable.java
@@ -0,0 +1,15 @@
+package net.MUI2.future;
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import net.minecraft.nbt.NBTBase;
+
+public interface INBTSerializable<T extends NBTBase> {
+
+    T serializeNBT();
+
+    void deserializeNBT(T nbt);
+}
